9f6c516d361c4715543876ac395c787829ff1268
ShlokModi <65662601+ShlokModi@users.noreply.github.com>
2020-07-08 17:05:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@ var Engine = Matter.Engine,
   World = Matter.World,
   Events = Matter.Events,
   Bodies = Matter.Bodies;
- 
 var particles = [];
 var plinkos = [];
 var divisions = [];
+
 var divisionHeight=300;
 var score =0;
 var particle;
@@ -90,28 +90,70 @@ function draw() {
      
      divisions[k].display();
    }
-   if(particle!==null)
+   if(particle!==undefined)
    {
      particle.display();
 
      if(particle.body.position.y>760)
      {
-       if(particle.body.position.x < 300)
+       if(particle.body.position.x < 80)
        {
-         score=score+500;
-         particle=null;
+         score=score+150;
+         particle=undefined;
          if( count>= 5) gameState = "end";
        }
-        if(particle.body.position.x > 601 && particle.body.position.x < 900)
+        else if(particle.body.position.x > 81 && particle.body.position.x < 160)
        {
          score=score+200;
-         particle=null;
+         particle=undefined;
          if( count>= 5) gameState = "end";
        }
-        if(particle.body.position.x > 301 && particle.body.position.x < 600)
+       else if(particle.body.position.x > 161 && particle.body.position.x < 240)
+       {
+         score=score+350;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 241 && particle.body.position.x < 320)
+       {
+         score=score+400;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 321 && particle.body.position.x < 400)
+       {
+         score=score+500;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 401 && particle.body.position.x < 480)
+       {
+         score=score+450;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 481 && particle.body.position.x < 560)
+       {
+         score=score+300;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 561 && particle.body.position.x < 640)
+       {
+         score=score+250;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 641 && particle.body.position.x < 720)
        {
          score=score+100;
-         particle=null;
+         particle=undefined;
+         if( count>= 5) gameState = "end";
+       }
+       else if(particle.body.position.x > 721 && particle.body.position.x < 800)
+       {
+         score=score+50;
+         particle=undefined;
          if( count>= 5) gameState = "end";
        }
      }
